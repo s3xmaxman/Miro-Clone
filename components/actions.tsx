7579b0f052bfcc1d43eeccaae3ce183c6d54fc91
s3xmaxman <badside3919@gmail.com>
2024-04-08 08:45:25 +0900

    add confirm-modal

--- components/actions.tsx
+++ components/actions.tsx
@@ -7,10 +7,13 @@ import {
     DropdownMenuItem,
     DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
-
-  import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
-import { Link2 } from "lucide-react";
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
+import { ConfirmModal } from "./confirm-modal";
+import { Button } from "./ui/button";
 
   interface ActionsProps {
     children: React.ReactNode;
@@ -23,6 +26,19 @@ import { toast } from "sonner";
 
 export const Actions = ({ children, side, sideOffset, id, title }: ActionsProps) => {
 
+    const { mutate, pending } = useApiMutation(api.board.remove);
+    
+    const onDelete = () => {
+        mutate({ id })
+        .then(() => {
+            toast.success(`${title}を削除しました`);
+        })
+        .catch(() => {
+            toast.error(`${title}の削除に失敗しました`);
+        })
+    }
+
+
     const onCopyLink = () => {
         navigator.clipboard.writeText(`${window.location.origin}/board/${id}`)
         .then(() => {
@@ -32,6 +48,7 @@ export const Actions = ({ children, side, sideOffset, id, title }: ActionsProps)
             toast.error(`${title}のリンクのコピーに失敗しました`);
         })
     }
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -50,6 +67,17 @@ export const Actions = ({ children, side, sideOffset, id, title }: ActionsProps)
                     <Link2 className="h-4 w-4 mr-2" />
                         リンクをコピー 
                 </DropdownMenuItem>
+                <ConfirmModal
+                   header="削除"
+                   description={`${title}を削除します。よろしいですか?`}
+                   disabled={pending}
+                   onConfirm={onDelete}
+                >
+                    <Button variant="ghost" className="p-3 cursor-pointer text-sm w-full justify-start font-normal">
+                        <Trash2 className="h-4 w-4 mr-2" />
+                            {title}を削除
+                    </Button>
+                </ConfirmModal>
             </DropdownMenuContent> 
         </DropdownMenu>       
     );
