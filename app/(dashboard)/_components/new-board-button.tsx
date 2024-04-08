f9f089a1b3142124d504b679de9f1c007bf1d3ec
s3xmaxman <badside3919@gmail.com>
2024-04-08 15:33:39 +0900

    progress

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -4,6 +4,7 @@ import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 interface NewBoardButtonProps {
@@ -16,6 +17,7 @@ export const NewBoardButton = ({
     orgId,
     disabled
 }: NewBoardButtonProps) => {
+    const router = useRouter();
     const { mutate, pending } = useApiMutation(api.board.create);
 
     const onClick = () => {
@@ -25,6 +27,7 @@ export const NewBoardButton = ({
         })
         .then((id) => {
             toast.success("ボードを作成しました");
+            router.push(`/board/${id}`);
         })
         .catch(() => {
             toast.error("ボードの作成に失敗しました");
