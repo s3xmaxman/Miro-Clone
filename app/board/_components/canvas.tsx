2ca075d79a6740699b229c2a33eae51704732321
s3xmaxman <badside3919@gmail.com>
2024-04-09 14:33:44 +0900

    add live mouse pointer move

--- app/board/_components/canvas.tsx
+++ app/board/_components/canvas.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { CanvasMode, CanvasState } from "@/types/canvas";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { useHistory, useSelf, useCanRedo, useCanUndo }  from "@/liveblocks.config"
+import { useHistory, useSelf, useCanRedo, useCanUndo, useMutation }  from "@/liveblocks.config"
+import { CursorsPresence } from "./cursors-presence";
+import { pointerEventToCanvasPoint } from "@/lib/utils";
 
 interface CanvasProps {
     boardId: string
@@ -17,11 +19,32 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         mode: CanvasMode.None
     });
 
+    const [camera, setCamera] = useState({ x: 0, y: 0 });
+
     const info = useSelf((me) => me.info);
 
     const history = useHistory();
     const canUndo = useCanUndo();
     const canRedo = useCanRedo();
+
+    const onWheel = useCallback((e: React.WheelEvent) => {
+        setCamera((camera) => ({
+           x: camera.x - e.deltaX, 
+           y: camera.y - e.deltaY
+        }))
+    }, []);
+
+    const onPointerMove = useMutation(({ setMyPresence }, e: React.PointerEvent) => {
+        e.preventDefault();
+
+        const current = pointerEventToCanvasPoint(e, camera);
+
+        setMyPresence({ cursor: current });
+    }, [])
+
+    const onPointerLeave = useMutation(({ setMyPresence }) => {
+        setMyPresence({ cursor: null });
+    }, [])
    
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
@@ -35,6 +58,16 @@ export const Canvas = ({ boardId }: CanvasProps) => {
                 redo={history.redo}
                 undo={history.undo} 
             />
+            <svg
+                className="h-[100vh] w-[100vw]"
+                onWheel={onWheel}
+                onPointerMove={onPointerMove}
+                onPointerLeave={onPointerLeave}
+            >
+                <g>
+                  <CursorsPresence />  
+               </g>
+            </svg>
         </main>
     )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+import { Camera } from "@/types/canvas";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -17,3 +18,13 @@ const COLORS = [
 export function connectionIdToColor(connectionId: number) {
   return COLORS[connectionId % COLORS.length];
 }
+
+export function pointerEventToCanvasPoint(
+  e: React.PointerEvent,
+  camera: Camera,
+) {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+};
