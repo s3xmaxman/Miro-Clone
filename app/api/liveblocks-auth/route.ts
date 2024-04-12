cda5a51e3d13467f5936379621e33fdb67c7cf70
s3xmaxman <badside3919@gmail.com>
2024-04-12 11:04:17 +0900

    fix

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,16 +1,19 @@
-import { api } from "@/convex/_generated/api";
 import { auth, currentUser } from "@clerk/nextjs";
 import { Liveblocks } from "@liveblocks/node";
 import { ConvexHttpClient } from "convex/browser";
 
-const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+import { api } from "@/convex/_generated/api";
+
+const convex = new ConvexHttpClient(
+    process.env.NEXT_PUBLIC_CONVEX_URL!
+);
 
 const liveblocks = new Liveblocks({
     secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
 export async function POST(request: Request) {
-    const authorization = auth();
+    const authorization = await auth();
     const user = await currentUser();
 
     if (!authorization || !user) {
@@ -29,13 +32,15 @@ export async function POST(request: Request) {
         picture: user.imageUrl,
     };
 
-    const session = liveblocks.prepareSession(user.id, { userInfo });
+    const session = liveblocks.prepareSession(
+        user.id,
+        { userInfo }
+    );
 
     if (room) {
         session.allow(room, session.FULL_ACCESS);
     }
 
     const { status, body } = await session.authorize();
-
     return new Response(body, { status });
-}
+};
